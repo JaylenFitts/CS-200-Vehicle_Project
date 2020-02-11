d40ffcf759f1c204db3c8b073a9af5d31f8ec6ca
Jaylen Fitts <37253748+JaylenFitts@users.noreply.github.com>
2020-02-11 16:12:00 -0500

    clean code

--- main.cpp
+++ main.cpp
@@ -1,35 +1,37 @@
 #include <iostream>
 #include "Vehicle.h"
+
 using namespace std;
 
-int main(int argc, char *argv[]){
+int main(){
     Vehicle honda_civic = Vehicle();
-    cout << "Honda Civic Gas Level: " << honda_civic.getGasLevel();
-    cout << "Honda Civic Distance: " << honda_civic.getDistTraveled();
+    cout << "Honda Civic Gas Level: " << honda_civic.getGasLevel() << endl;
+    cout << "Honda Civic Distance: " << honda_civic.getDistTraveled() << endl;
 
     honda_civic.setDistTraveled(10);
 
-    cout << "Honda Civic Gas Level: " <<  honda_civic.getGasLevel();
-    cout << "Honda Civic Distance: " <<  honda_civic.getDistTraveled();
+    cout << "Honda Civic Gas Level: " <<  honda_civic.getGasLevel() << endl;
+    cout << "Honda Civic Distance: " <<  honda_civic.getDistTraveled() << endl;
 
     Vehicle nissan_sentra = Vehicle(20.0, 4.0);
-    cout << "Nissan Sentra Gas Level: " << nissan_sentra.getGasLevel();
-    cout << "Nissan Sentra Distance: " << nissan_sentra.getDistTraveled();
+    cout << "Nissan Sentra Gas Level: " << nissan_sentra.getGasLevel() << endl;
+    cout << "Nissan Sentra Distance: " << nissan_sentra.getDistTraveled() << endl;
 
     nissan_sentra.fillTank(10);
     nissan_sentra.setDistTraveled(10);
 
-    cout << "Nissan Sentra Gas Level: " << nissan_sentra.getGasLevel();
-    cout << "Nissan Sentra Distance: " << nissan_sentra.getDistTraveled();
+    cout << "Nissan Sentra Gas Level: " << nissan_sentra.getGasLevel() << endl;
+    cout << "Nissan Sentra Distance: " << nissan_sentra.getDistTraveled() << endl;
 
     nissan_sentra.fillTank(30);
     nissan_sentra.setDistTraveled(10);
-    cout << "Nissan Sentra Gas Level: " << nissan_sentra.getGasLevel();
-    cout << "Nissan Sentra Distance: " << nissan_sentra.getDistTraveled();
+    cout << "Nissan Sentra Gas Level: " << nissan_sentra.getGasLevel() << endl;
+    cout << "Nissan Sentra Distance: " << nissan_sentra.getDistTraveled() << endl;
     
     nissan_sentra.setDistTraveled(100);
-    cout << "Nissan Sentra Gas Level: " << nissan_sentra.getGasLevel();
-    cout << "Nissan Sentra Distance: " << nissan_sentra.getDistTraveled();
+    cout << "Nissan Sentra Gas Level: " << nissan_sentra.getGasLevel() << endl;
+    cout << "Nissan Sentra Distance: " << nissan_sentra.getDistTraveled() << endl;
 
+    system("PAUSE");
     return 0;
 }
